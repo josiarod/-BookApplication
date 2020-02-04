c973815039503744d51959cbad8e5e562deebe12
GBTC440011UR <GBTC440011UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-04 12:33:22 -0500

    getDisplay

--- src/BookDB.java
+++ src/BookDB.java
@@ -55,10 +55,52 @@
        }
 
      public void book(){
-         System.out.println("Title: " + title);
-         System.out.println("Author: " + author);
-         System.out.println("Description: " + description);
-         System.out.println("Price: " + price);
+         System.out.println("Title: " + getTitle());
+         System.out.println("Author: " + getAuthor());
+         System.out.println("Description: " + getDescription());
+         System.out.println("Price: " + getPrice());
          System.out.println();
      }
+
+       public String getSku() {
+           return sku;
+       }
+
+       public void setSku(String sku) {
+           this.sku = sku;
+       }
+
+       public String getTitle() {
+           return title;
+       }
+
+       public void setTitle(String title) {
+           this.title = title;
+       }
+
+       public String getAuthor() {
+           return author;
+       }
+
+       public void setAuthor(String author) {
+           this.author = author;
+       }
+
+       public String getDescription() {
+           return description;
+       }
+
+       public void setDescription(String description) {
+           this.description = description;
+       }
+
+       public double getPrice() {
+           return price;
+       }
+
+       public void setPrice(double price) {
+           this.price = price;
+       }
+
+
    }
